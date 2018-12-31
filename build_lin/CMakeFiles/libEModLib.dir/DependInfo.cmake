c90853685c91165f6c74e162a7da03ef8a2e18fa
PJercic <petar.jercic@gmail.com>
2018-12-31 19:39:05 +0100

    Add Linux build

---
+++ build_lin/CMakeFiles/libEModLib.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/petar/Development/EModLib/src/Affect.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Affect.cpp.o"
+  "/home/petar/Development/EModLib/src/EmotionsModelLib.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/EmotionsModelLib.cpp.o"
+  "/home/petar/Development/EModLib/src/Empath.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Empath.cpp.o"
+  "/home/petar/Development/EModLib/src/Gusher.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Gusher.cpp.o"
+  "/home/petar/Development/EModLib/src/Intellectual.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Intellectual.cpp.o"
+  "/home/petar/Development/EModLib/src/Mood.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Mood.cpp.o"
+  "/home/petar/Development/EModLib/src/PeopleFactory.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/PeopleFactory.cpp.o"
+  "/home/petar/Development/EModLib/src/Personality.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Personality.cpp.o"
+  "/home/petar/Development/EModLib/src/Rock.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/Rock.cpp.o"
+  "/home/petar/Development/EModLib/src/stimulus.cpp" "/home/petar/Development/EModLib/build_lin/CMakeFiles/libEModLib.dir/src/stimulus.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_lin/CMakeFiles/libEModLib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libEModLib.dir/src/stimulus.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Affect.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Mood.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Personality.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Rock.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Gusher.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Empath.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/Intellectual.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/PeopleFactory.cpp.o"
+  "CMakeFiles/libEModLib.dir/src/EmotionsModelLib.cpp.o"
+  "liblibEModLib.pdb"
+  "liblibEModLib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libEModLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
