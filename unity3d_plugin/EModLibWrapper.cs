2c430505d3cc96d9e76429492b6a677c84600dc4
Petar Jerčić <petar.jercic@gmail.com>
2019-01-13 20:36:22 +0100

    Add Unity3D plugin wrapper for the library
    Add wrapper file for the native unmanaged C++ code, and a script example using it.

---
+++ unity3d_plugin/EModLibWrapper.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Runtime.InteropServices;
+
+public class EModLibWrapper
+{
+#if UNITY_STANDALONE_WIN
+    const string dll = "libEModLib";
+#elif UNITY_STANDALONE_OSX
+    const string dll = "PluginUnmanagedTestBundle";
+#elif UNITY_STANDALONE_LIN
+    const string dll = "PluginUnmanagedTestLin";
+#elif UNITY_IOS
+    const string dll = "__Internal";
+#elif UNITY_ANDROID
+    const string dll = "???";
+#else
+    const string dll = " ";
+#endif
+
+    [DllImport(dll)]
+    private static extern void Instantiate();
+
+    [DllImport(dll)]
+    private static extern void inStimulus(int persId, double arousal, double valence);
+
+    [DllImport(dll)]
+    private static extern int getEmotion(int persId);
+
+    [DllImport(dll)]
+    private static extern int createPerson(int type);
+
+    [DllImport(dll)]
+    private static extern void Start();
+
+    [DllImport(dll)]
+    private static extern void Stop();
+
+    public static void InstantiateEx()
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LIN)
+        Instantiate();
+#endif
+    }
+
+    public static void inStimulusEx(int persId, double arousal, double valence)
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LIN)
+        inStimulus(persId, arousal, valence);
+#endif
+    }
+
+    public static int getEmotionEx(int persId)
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LIN)
+        return getEmotion(persId);
+#else
+        return -1;
+#endif
+    }
+
+    public static int createPersonEx(int type)
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LIN)
+        return createPerson(type);
+#else
+        return -1;
+#endif
+    }
+
+    public static void StartEx()
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LIN)
+        Start();
+#endif
+    }
+
+    public static void StopEx()
+    {
+#if (UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LIN)
+        Stop();
+#endif
+    }
+}
---
+++ unity3d_plugin/PluginUnmanagedTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Runtime.InteropServices;
+
+public class PluginUnmanagedTest : MonoBehaviour
+{
+    void Start()
+    {
+        EModLibWrapper.InstantiateEx();
+        EModLibWrapper.createPersonEx(0);
+        EModLibWrapper.inStimulusEx(0, 0.5, 0.5);
+        EModLibWrapper.StartEx();
+    }
+
+    void Update()
+    {
+        print("Native random number " + EModLibWrapper.getEmotionEx(0));
+    }
+}
