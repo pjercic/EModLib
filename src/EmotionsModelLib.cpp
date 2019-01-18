9ac2303ce00e152c89ad868b43f0a2d0345022f1
Petar Jerčić <petar.jercic@gmail.com>
2019-01-18 13:56:18 +0100

    Update EmotionsModelLib.cpp

--- src/EmotionsModelLib.cpp
+++ src/EmotionsModelLib.cpp
@@ -9,7 +9,24 @@
 #include "IPerson.h"
 
 #include <algorithm>
+/*
+extern "C" {
+    float TestMultiply(float a, float b)
+    {
+        return a * b;
+    }
 
+    float TestDivide(float a, float b)
+    {
+        if (b == 0) {
+            return 0;
+            //throw invalid_argument("b cannot be zero!");
+        }
+
+        return a / b;
+    }
+}
+*/
 EmotionsModel::EmotionsModelLib* gEModLib = nullptr;
 
 // Enternal Unmanaged API
